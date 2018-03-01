ed6bed68484e8faaa61a5cb2dd4b18d4efe7c861
martin-raven <martinsiby@gmail.com>
2018-03-01 22:04:48 +0530

    permissionn fix

--- app/src/main/java/com/envyus/goclean/MainActivity.java
+++ app/src/main/java/com/envyus/goclean/MainActivity.java
@@ -1,11 +1,13 @@
 package com.envyus.goclean;
-//raven in
 
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentManager;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 import android.widget.TextView;
@@ -13,14 +15,14 @@ import android.widget.TextView;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
 //suhail
     private TextView mTextMessage;
     private FirebaseAuth mAuth;
-
-
-
-
+    public static final int REQUEST_ID_MULTIPLE_PERMISSIONS = 1;
 //
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,6 +32,7 @@ public class MainActivity extends AppCompatActivity {
         manager.beginTransaction().replace(R.id.container, new cameraview()).commit();
         mAuth = FirebaseAuth.getInstance();
         FirebaseUser currentUser = mAuth.getCurrentUser();
+        while(!checkAndRequestPermissions()){}
         if(currentUser==null)
         {
             Intent GotoLogin=new Intent(this,LoginActivity.class);
@@ -73,6 +76,28 @@ public class MainActivity extends AppCompatActivity {
     public void switchToFragment3() {
         FragmentManager manager3 = getSupportFragmentManager();
         manager3.beginTransaction().replace(R.id.container, new profileview()).commit();
+    }
+    private  boolean checkAndRequestPermissions() {
+        int internet=ContextCompat.checkSelfPermission(this, android.Manifest.permission.INTERNET);
+        int loc = ContextCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION);
+        int loc2 = ContextCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION);
+        List<String> listPermissionsNeeded = new ArrayList<>();
+        if (internet != PackageManager.PERMISSION_GRANTED) {
+            listPermissionsNeeded.add(android.Manifest.permission.WRITE_EXTERNAL_STORAGE);
+        }
+        if (loc2 != PackageManager.PERMISSION_GRANTED) {
+            listPermissionsNeeded.add(android.Manifest.permission.ACCESS_FINE_LOCATION);
+        }
+        if (loc != PackageManager.PERMISSION_GRANTED) {
+            listPermissionsNeeded.add(android.Manifest.permission.ACCESS_COARSE_LOCATION);
+        }
+        if (!listPermissionsNeeded.isEmpty())
+        {
+            ActivityCompat.requestPermissions(this,listPermissionsNeeded.toArray
+                    (new String[listPermissionsNeeded.size()]),REQUEST_ID_MULTIPLE_PERMISSIONS);
+            return false;
+        }
+        return true;
     }
 
 }
